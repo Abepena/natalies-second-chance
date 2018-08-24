e3885abf20ae50d759ccde7c21a10438f9ad563b
Abe Pena <pena.abe@gmail.com>
2018-08-23 22:11:07 -0400

    fixed payment modal not displaying

--- natalies_second_chance_project/static/js/index.js
+++ natalies_second_chance_project/static/js/index.js
@@ -1,12 +1,45 @@
-// Change the color of each of the headers
+
+//smoothScoll function learned from https://www.youtube.com/watch?v=oUSvlrDTLi
 function smoothScroll(target, duration) {
     var target = document.querySelector(target)
-    console.log(target);
-    console.log(duration);
+    var targetPosition = target.getBoundingClientRect().top - 70; // top of target div
+    var startPosition = window.pageYOffset; // postion from the top of the page when the page is loaded
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime
+        var run = ease(timeElapsed, startPosition, targetPosition, duration)
+        window.scroll(0, run)
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+
+    }
+
+    // ease function from http://gizma.com/easing/
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--; 
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation); //makes our animation loop smoothly at 60fps
 }
 
-smoothScroll('#outreach', 1000)
+let navigationLinks = document.querySelectorAll('.main-nav-link')
 
+for (let e of navigationLinks){
+    let link = e.getAttribute('href');
+
+    if (link != '#') {
+        e.addEventListener('click', function(){
+            smoothScroll(link, 750)
+        })
+    }
+
+}
+
+// Change the color of each of the headers
 const adoptableHeaders = document.getElementsByClassName('adopt-header')
 
 let count = 0
