17180b391d46507ba2754c26d16a67d4b21b0e93
Abe Pena <pena.abe@gmail.com>
2018-10-18 15:50:20 -0400

    setup for digital ocean deployment

--- natalies_second_chance_project/natalies_second_chance/settings.py
+++ natalies_second_chance_project/natalies_second_chance/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+
+import dj_database_url
 from decouple import config , Csv
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -87,10 +90,7 @@ WSGI_APPLICATION = 'natalies_second_chance.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
