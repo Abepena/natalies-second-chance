acd60fdb18b6b70f1681544860df76ad4839b492
Abe Pena <pena.abe@gmail.com>
2018-10-19 02:12:34 -0400

    added delete button to home page, dog list, and detail view

--- natalies_second_chance_project/dogs/urls.py
+++ natalies_second_chance_project/dogs/urls.py
@@ -8,8 +8,10 @@ app_name = 'dogs'
 urlpatterns = [
     path('', views.DogListView.as_view(), name='list'),
     path('<int:pk>/', views.DogDetailView.as_view(), name='detail'),
+    path('<int:pk>/delete', views.DogDeleteView.as_view(), name='delete'),
     path('create/', views.DogCreateView.as_view(), name='create' ),
     path('update/<int:pk>', views.DogUpdateView.as_view(), name='update'),
     
 ]
+
 
